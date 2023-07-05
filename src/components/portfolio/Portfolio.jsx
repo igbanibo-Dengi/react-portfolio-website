52a57a406df15eee57ba0515645a04f11ecbd762
dengi <mcmorgan6560@gmail.com>
2023-07-06 00:56:04 +0100

    removed unused vars

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -11,11 +11,9 @@ import DippsImg from "../../assets/dipps.png";
 import { ImHtmlFive2 } from "react-icons/im";
 import { RiCss3Fill } from "react-icons/ri";
 import { IoLogoJavascript } from "react-icons/io";
-import { BiGitMerge } from "react-icons/bi";
 import { FaReact } from "react-icons/fa";
 import { SiTailwindcss } from "react-icons/si";
 import { TbBrandBootstrap } from "react-icons/tb";
-import { TbBrandTypescript } from "react-icons/tb";
 import { TbBrandFramerMotion } from "react-icons/tb";
 
 const data = [
